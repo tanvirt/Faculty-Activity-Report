b1ba2da2e9dca2f7e835b95df6d24e68795b0553
dafefuve <dafefuve@DAFEFUVE-MACPRO.local>
2014-09-27 14:31:06 -0400

    [Modified/Created  #79327036] working on integrating ng-grid with assigned activity

--- public/modules/reports/controllers/assigned-activity.client.controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-//assigned activity controller
-angular.module('reports').controller('AssignedActivityController', ['$scope',
-	function($scope) {
-		// Assigned activity controller logic
-		// ...
-	}
-]);
