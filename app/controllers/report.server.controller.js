41f3f2423e59144fec70eabe536596ac99414665
mkwheeler <mkwheeler@ufl.edu>
2014-10-01 23:05:43 -0400

    added title and signature to controller

--- app/controllers/report.server.controller.js
+++ app/controllers/report.server.controller.js
@@ -18,13 +18,20 @@ exports.latexString = function(req,res,next) {
 	var writeable = fs.createWriteStream('./app/pdf/report.pdf');
 	latex([
 		'\\documentclass{article}',
-		'\\begin{document}',	
+		'\\begin{document}',
+		'\\title{COLLEGE OF ENGINEERING \\ Annual Activities Report}',
+		'\\date{}',
+		'\\maketitle',
 		renderName.render(),
 		renderTenure.render(),
 		//assignedActivity.render(),
 		//teachingAdvising.render(),
 		//teachingEvaluation.render(),
 		//dateAppointed.render(),
+		'\\vspace{2in}',
+		'\\begin{center}',
+		'Signature', '\\line(1,0){200}', '\\hspace{1em}', 'Date', '\\line(1,0){50}',
+		'\\end{center}',
 		'\\end{document}'
 	]).pipe(writeable); //print to console for debugging
 	next();
