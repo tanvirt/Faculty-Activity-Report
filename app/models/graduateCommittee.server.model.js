46f149d405910d7d76b6303c31f904a3e7821389
unknown <aknoxlor@gmail.com>
2014-10-03 16:41:02 -0400

    Created schema for Graduate Commitee Activity table

---
+++ app/models/graduateCommittee.server.model.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * A Validation function for local mean properties
+ */
+var validateLocalStrategyMean = function(property) {
+	if(property.length !== 9)
+		return false;
+	for(var i = 0; i < 9; i++)
+	{
+		if((property[i] < this.lowScore) || (property[i] > this.highScore))
+			return false;
+	}
+	
+	return true;
+};
+
+//The count of each role will be calculated on demand by using the MongoDB count command
+var graduateCommittee = new Schema({
+
+	role: {
+		type: String,
+		enum: ['Chair','Co-Chair','External','Member','Minor'],
+		required: true
+	},
+	studentName: {
+		type: String,
+		required: true
+	},
+	degree: {
+		type: String,
+		enum: ['M.S.','Ph.D.'], //I have a feeling this will need to be expanded
+		required: true
+	},
+	degreeDate: {
+		type: Date 
+	}
+}, {collection:'GraduateCommittee'});
+
+mongoose.model('GraduateCommittee', graduateCommittee);
