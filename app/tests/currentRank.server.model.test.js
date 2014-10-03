c7108ac806f1431a88299d63bff9aed9d5a26496
Christine King <airpwane@ufl.edu>
2014-10-03 11:38:20 -0400

    Create currentRank.server.model.test.js

---
+++ app/tests/currentRank.server.model.test.js
@@ -0,0 +1,50 @@
+'use strict';
+/**
+ * Module dependencies.
+ */
+var should = require('should'),
+    mongoose = require('mongoose'),
+    currentRank = mongoose.model('currentRank');
+/**
+ * Globals
+ */
+var rank01;
+describe('currentRank Model Unit Tests:', function() {
+    beforeEach(function(done) {
+        rank01 = new currentRank({
+            rank: 'professor',
+            department: 'Computer and Informational Science and Engineering'
+
+        });
+
+        done();
+    });
+    describe('Method Save', function() {
+        it('should be able to save without problem', function(done) {
+            rank01.save();
+            done();
+        });
+
+        it('should fail to save if the rank is not one of the possible ranks', function(done) {
+            rank01.rank = 'asdf';
+            return rank01.save(function(err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+        it('should fail to save if the department is not one of the possible departments', function(done) {
+            rank01.department = 'asdf';
+            return rank01.save(function(err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+
+    });
+    afterEach(function(done) {
+        currentRank.remove().exec();
+        done();
+    });
+});
