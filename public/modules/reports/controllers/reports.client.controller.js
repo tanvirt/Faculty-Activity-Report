73d7c917ec6ef4bb6472067eef805549a1aa1172
dafefuve <root@DAFEFUVE-MACPRO.local>
2014-10-05 19:51:28 -0400

    [Modified/Created  #79327036] functinality is defined it need to be properly linked with the controller

--- public/modules/reports/controllers/reports.client.controller.js
+++ public/modules/reports/controllers/reports.client.controller.js
@@ -5,13 +5,40 @@ angular.module('reports').controller('ReportsController', ['$scope', '$statePara
 	function($scope, $stateParams, $location, Authentication, Reports ) {
 		$scope.authentication = Authentication;
 
-        $scope.updateFall = function()
-        {
-            this.totalSpring =  44;
 
+        $scope.springUpdate = function()
+        {
+             $scope.springTotal = $scope.springTeaching + $scope.springResearch + $scope.springService;
         };
 
-        //$scope.teachingSpring = TeachingFall;
+        $scope.summerUpdate = function()
+        {
+            $scope.summerTotal = $scope.summerTeaching + $scope.summerResearch + $scope.summerService;
+        };
+
+        $scope.fallUpdate = function()
+        {
+            $scope.fallTotal = $scope.fallTeaching + $scope.fallResearch + $scope.fallService;
+        };
+        $scope.initTable = function(){
+            //Spring values
+            $scope.springTeaching = 0;
+            $scope.springResearch = 0;
+            $scope.springService = 0;
+            //Summer values
+
+            $scope.summerTeaching = 0;
+            $scope.summerResearch = 0;
+            $scope.summerService = 0;
+            //Fall Values
+            $scope.fallTeaching = 0;
+            $scope.fallResearch = 0;
+            $scope.fallService = 0;
+            //table totals
+            $scope.springTotal = 0;
+            $scope.summerTotal = 0;
+            $scope.fallTotal = 0;
+        };
 
 		// Create new Report
 		$scope.create = function() {
