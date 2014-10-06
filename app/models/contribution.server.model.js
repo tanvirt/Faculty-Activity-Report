a01995e0dea9e91e1c9ca62f5458b52a451ce891
Greg <friedmangreg37@ufl.edu>
2014-10-06 16:18:33 -0400

    Added tests for contributions section

--- app/models/contribution.server.model.js
+++ app/models/contribution.server.model.js
@@ -7,17 +7,41 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
 	validator = require('validator');
 
+/**
+* Checks that website is valid
+*/
 var validateWebsite = function(p) {
 	return validator.isURL(p);
 };
 
 /**
- * Checks that citations numbers are greater than or equal to zero
+ * Checks that numbers are greater than or equal to zero
  */
-var validateLocalStrategyProperty = function(property) {
+var validateNumbers = function(property) {
 	return property >= 0;
 };
 
+/**
+* Checks that length of string does not exceed 500 words
+
+var validateStringLength500 = function(property) {
+	return (property.length <= 500);
+};
+
+/**
+* Checks that length of string does not exceed 250 words
+
+var validateStringLength250 = function(property) {
+	return (property.length <= 250);
+};
+
+/**
+* Checks that length of titles does not exceed 50 words
+
+var validateTitleLength = function(property) {
+	return (property.length <= 50);
+};
+*/
 var Contribution = new Schema({
 	intro: {
 		type: String,
@@ -25,8 +49,7 @@ var Contribution = new Schema({
 	},
 	examples: {
 		title: {
-			type: String,
-			default: ''
+			type: String
 		},
 		body: {
 			type: String,
@@ -44,21 +67,27 @@ var Contribution = new Schema({
 		citations: {
 			type: Number,
 			required: true,
-			validate: [validateLocalStrategyProperty,
+			validate: [validateNumbers,
 					'Number must be greater than or equal to zero']
 		}
 	},
 	totalCitations: {
 		type: Number,
-		required: true
+		required: true,
+		validate: [validateNumbers,
+					'Number must be greater than or equal to zero']
 	},
 	h_index: {
 		type: Number,
-		required: true
+		required: true,
+		validate: [validateNumbers,
+					'Number must be greater than or equal to zero']
 	},
 	i_index: {
 		type: Number,
-		required: true
+		required: true,
+		validate: [validateNumbers,
+					'Number must be greater than or equal to zero']
 	},
 	website: {
 		type: String,
