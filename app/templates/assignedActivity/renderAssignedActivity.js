c403743b4f503b96a7347555d54bd53f48aacd0e
mkwheeler <mkwheeler@ufl.edu>
2014-10-06 13:32:31 -0400

    render+ original model of assignedActivity

--- app/templates/assignedActivity/renderAssignedActivity.js
+++ app/templates/assignedActivity/renderAssignedActivity.js
@@ -9,28 +9,32 @@ var AssignedActivity = mongoose.model('AssignedActivity');
 /*
 Populates the database with test data
 */
-function dummyObject(Model) {
-	var obj = new Model({
+function dummyObjects() {
+	var objs = [],
+	
+	objs[0] = new AssignedActivity({
 		year: 2014,
-		
-		fall: {
-			teaching: 45,
-			research: 40,
-			service: 15
-		},
-		spring: {
-			teaching: 45,
-			research: 40,
-			service: 15
-		},
-		summer: {
-			teaching: 45,
-			research: 40,
-			service: 15
-		}
-		
+		semester: 'fall',
+		teaching: 30,
+		research: 40,
+		service: 30
+	)};
+	objs[1] = new AssignedActivity({
+		year: 2014,
+		semester: 'spring',
+		teaching: 20,
+		research: 50,
+		service: 30
+	)};
+	objs[2] = new AssignedActivity({
+		year: 2014,
+		semester: 'summer',
+		teaching: 10,
+		research: 70,
+		service: 20
 	});
-	return obj;
+	
+	return objs;
 }
 
 /*
@@ -38,7 +42,7 @@ Helper function that gets called in report.server.controller.js
 Output is pushed into a LaTex PDF there.
 */
 module.exports.render = function (callback) {
-	renderModel.render( 'assignedActivity/assignedActivity.tex', AssignedActivity, dummyObject, function ( renderStr ) {
+	renderModel.render( 'assignedActivity/assignedActivity.tex', AssignedActivity, {}, passObj, dummyObjects, function ( renderStr ) {
 		callback(null, renderStr);
 	});
 };
