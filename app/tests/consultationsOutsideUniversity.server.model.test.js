df84eba8408a4fd817ee25adf3798576147a2737
Christine King <airpwane@ufl.edu>
2014-10-06 23:19:38 -0400

    Create consultationsOutsideUniversity.server.model.test.js

---
+++ app/tests/consultationsOutsideUniversity.server.model.test.js
@@ -0,0 +1,42 @@
+'use strict';
+/**
+ * Module dependencies.
+ */
+var should = require('should'),
+    mongoose = require('mongoose'),
+    consultationsOutsideUniversity = mongoose.model('consultationsOutsideUniversity');
+/**
+ * Globals
+ */
+var con01;
+describe('consultationsOutsideUniversity Model Unit Tests:', function() {
+    beforeEach(function(done) {
+        con01 = new consultationsOutsideUniversity({
+            consultation: 'N/A'
+
+        });
+
+        done();
+    });
+    describe('Method Save', function() {
+        it('should be able to save without problem', function(done) {
+            con01.save();
+            done();
+        });
+
+        it('should fail to save if the field is empty', function(done) {
+            con01.consultation = '';
+            return con01.save(function(err) {
+                should.exist(err);
+                done();
+            });
+        });
+
+
+    });
+    afterEach(function(done) {
+        consultationsOutsideUniversity.remove().exec();
+        done();
+    });
+});
+
