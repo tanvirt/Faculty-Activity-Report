8eb7004173ff3192bddf9616398510cb7757a1b3
Pablo <pabloaran17@gmail.com>
2014-10-12 18:09:26 -0400

    Changes for release

--- public/modules/reports/controllers/reports.client.controller.js
+++ public/modules/reports/controllers/reports.client.controller.js
@@ -20,49 +20,26 @@ app.controller('ReportsController', ['$scope', '$stateParams', '$location', 'Aut
 		};
 
 		$scope.authentication = Authentication;
-
 		
-		//variable for section 15 to initialize the table
-		$scope.incomplete = false;
-		$scope.hideTable = true;
-		$scope.grants = 
-         	[{
-         		titleOfGrant:null,
-         		fundingAgency:null,
-         		PI:null,
-         		startEnd:null,
-         		value:null,
-         	}];
-        $scope.addGrants = function(){
-         		$scope.grants.push({titleOfGrant: $scope.grants.titleOfGrant , fundingAgency: $scope.grants.fundingAgency, PI: $scope.grants.PI, value:$scope.grants.value, startEnd: $scope.grants.startEnd});
-         		$scope.grants.titleOfGrant =null;
-         		$scope.grants.fundingAgency =null;
-         		$scope.grants.PI =null;
-         		$scope.grants.value=null;
-         		$scope.grants.startEnd=null;
-         		$scope.hideTable = false;
-
-         	};
-
-        $scope.$watch('grants.titleOfGrant',function(){$scope.test();});
-		$scope.$watch('grants.fundingAgency',function() {$scope.test();});
-		$scope.$watch('grants.PI',function() {$scope.test();});
-		$scope.$watch('grants.value',function() {$scope.test();});
-		$scope.$watch('grants.startEnd',function() {$scope.test();});
-
-		$scope.test = function() {
-			$scope.incomplete = true;
-			$scope.incomplete = false;
-			if($scope.grants.value <= 0 || !$scope.grants.titleOfGrant.length||!$scope.grants.fundingAgency.length||!$scope.grants.PI.length||!$scope.grants.startEnd.length){
-		     	$scope.incomplete = true;
-			}
-		};
-
+		
 		// Create new Report
 		$scope.create = function() {
 			// Create new Report object
 			var report = new Reports ({
-				reportName: this.reportName
+				reportName: this.reportName,
+
+				firstName: this.firstName,
+				middleName: this.middleName,
+				lastName: this.lastName,
+
+				tenure: this.tenure,
+
+				currentRank: this.currentRank,
+
+				dateAppointed: this.dateAppointed,
+
+				affiliateAppointments: this.affiliateAppointments,
+
 			});
 
 			// Redirect after save
