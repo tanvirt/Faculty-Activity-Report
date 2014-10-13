92953043aefe68174c132f1e6e15b36f78d6fb9c
Santiago <santiago7fergo@gmail.com>
2014-10-13 11:33:08 -0400

    Update section 15 controller and HTML

--- public/modules/reports/controllers/section-15.client.controller.js
+++ public/modules/reports/controllers/section-15.client.controller.js
@@ -6,16 +6,11 @@ angular.module('reports').controller('Section15Controller', ['$scope', '$statePa
 
 		$scope.authentication = Authentication;
 		
-<<<<<<< Updated upstream
+
 		//variable for section 15 to initialize the table
 		$scope.incomplete = false;
       $scope.hideTable = true;
-=======
-		//variables for section 15 to initialize the table
-		$scope.incomplete = false; //Checks if all the fields are complete in order to enable the add grant button
-      $scope.hideTable = true; 
 
->>>>>>> Stashed changes
       $scope.grants = 
             [{
                titleOfGrant:null,
@@ -24,7 +19,7 @@ angular.module('reports').controller('Section15Controller', ['$scope', '$statePa
                startEnd:null,
                value:null,
             }];
-<<<<<<< Updated upstream
+
         $scope.addGrants = function(){
                $scope.grants.push({titleOfGrant: $scope.grants.titleOfGrant , fundingAgency: $scope.grants.fundingAgency, PI: $scope.grants.PI, value:$scope.grants.value, startEnd: $scope.grants.startEnd});
                $scope.grants.titleOfGrant =null;
@@ -37,7 +32,7 @@ angular.module('reports').controller('Section15Controller', ['$scope', '$statePa
             };
 
         $scope.$watch('grants.titleOfGrant',function(){$scope.test();});
-=======
+
             
          //When the add grant button is pressed this function is called.
         $scope.addGrants = function(){
@@ -49,33 +44,24 @@ angular.module('reports').controller('Section15Controller', ['$scope', '$statePa
                $scope.grants.startEnd = null;
                $scope.hideTable = false;
             };
+            
        /*
-      //to watch whether these strings are empty, in order to check for completion of forms to enable button
-         $scope.test = function() {
-         $scope.incomplete = false;
-         if(!$scope.grants.value.length || !$scope.grants.titleOfGrant.length || !$scope.grants.fundingAgency.length || !$scope.grants.PI.length || !$scope.grants.startEnd.length){
-            $scope.incomplete = true;
-         }
-      }; 
       //Function to look for changes that are happening in the Grants object.
       $scope.$watch('grants.titleOfGrant',function(){$scope.test();});
->>>>>>> Stashed changes
       $scope.$watch('grants.fundingAgency',function() {$scope.test();});
       $scope.$watch('grants.PI',function() {$scope.test();});
       $scope.$watch('grants.value',function() {$scope.test();});
       $scope.$watch('grants.startEnd',function() {$scope.test();});
-<<<<<<< Updated upstream
 
+       //to watch whether these strings are empty, in order to check for completion of forms to enable button
       $scope.test = function() {
          $scope.incomplete = true;
          $scope.incomplete = false;
          if($scope.grants.value <= 0 || !$scope.grants.titleOfGrant.length||!$scope.grants.fundingAgency.length||!$scope.grants.PI.length||!$scope.grants.startEnd.length){
             $scope.incomplete = true;
          }
-      };
-=======
       */
 
->>>>>>> Stashed changes
+
 	}
 ]);
