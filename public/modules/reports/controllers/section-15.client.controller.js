d05298ef164dae23b888049ff9e06b9d94fafaf1
Santiago <santiago7fergo@gmail.com>
2014-10-13 09:47:41 -0400

    Update section 15 controller

--- public/modules/reports/controllers/section-15.client.controller.js
+++ public/modules/reports/controllers/section-15.client.controller.js
@@ -7,22 +7,39 @@ angular.module('reports').controller('Section15Controller', ['$scope', '$statePa
 		$scope.authentication = Authentication;
 		
 		//variable for section 15 to initialize the table
-		$scope.grants = 
-         	[{
-         		titleOfGrant:'Faculty Grants',
-         		fundingAgency:'EOF',
-         		PI:'Willy Nelson',
-         		startEnd:'01/13/2014',
-         		value:200	
-         	}];
+		$scope.incomplete = false;
+      $scope.hideTable = true;
+      $scope.grants = 
+            [{
+               titleOfGrant:null,
+               fundingAgency:null,
+               PI:null,
+               startEnd:null,
+               value:null,
+            }];
         $scope.addGrants = function(){
-         		$scope.grants.push({titleOfGrant: $scope.grants.titleOfGrant , fundingAgency: $scope.grants.fundingAgency, PI: $scope.grants.PI, value:$scope.grants.value, startEnd: $scope.grants.startEnd});
-         		$scope.grants.titleOfGrant = '';
-         		$scope.grants.fundingAgency = '';
-         		$scope.grants.PI = '';
-         		$scope.grants.value= '';
-         		$scope.grants.startEnd='';
-         	};
-		
+               $scope.grants.push({titleOfGrant: $scope.grants.titleOfGrant , fundingAgency: $scope.grants.fundingAgency, PI: $scope.grants.PI, value:$scope.grants.value, startEnd: $scope.grants.startEnd});
+               $scope.grants.titleOfGrant =null;
+               $scope.grants.fundingAgency =null;
+               $scope.grants.PI =null;
+               $scope.grants.value=null;
+               $scope.grants.startEnd=null;
+               $scope.hideTable = false;
+
+            };
+
+        $scope.$watch('grants.titleOfGrant',function(){$scope.test();});
+      $scope.$watch('grants.fundingAgency',function() {$scope.test();});
+      $scope.$watch('grants.PI',function() {$scope.test();});
+      $scope.$watch('grants.value',function() {$scope.test();});
+      $scope.$watch('grants.startEnd',function() {$scope.test();});
+
+      $scope.test = function() {
+         $scope.incomplete = true;
+         $scope.incomplete = false;
+         if($scope.grants.value <= 0 || !$scope.grants.titleOfGrant.length||!$scope.grants.fundingAgency.length||!$scope.grants.PI.length||!$scope.grants.startEnd.length){
+            $scope.incomplete = true;
+         }
+      };
 	}
 ]);
