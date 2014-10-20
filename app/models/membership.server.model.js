1a374811eb1a29eecb7c0db5c4687cfb134732cd
Greg <friedmangreg37@ufl.edu>
2014-10-20 15:12:18 -0400

    Added schema and tests for membership and activities section

---
+++ app/models/membership.server.model.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var validateLocalStrategyCourses = function(property) {
+	return property <= 2014 && property >= 1900;
+};
+
+var Membership = new Schema({
+	// add more arrays when you find out what else there can be
+	programCommittee: [{
+		title: {
+			type: String,
+			required: true
+		},
+		year: {
+			type: Number,
+			required: true,
+			validate: [validateLocalStrategyCourses, 
+			'Year must not be after current year or before 1900']
+		}
+	}]
+
+}, {collection:'Membership'});
+
+mongoose.model('Membership', Membership);
