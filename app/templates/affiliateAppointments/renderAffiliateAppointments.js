db32d691ed37876fadb3829646ef176eadcd4cf9
mkwheeler <mkwheeler@ufl.edu>
2014-10-24 18:10:41 -0400

    affiliateAppointment downloads

--- app/templates/affiliateAppointments/renderAffiliateAppointments.js
+++ app/templates/affiliateAppointments/renderAffiliateAppointments.js
@@ -1,30 +1,50 @@
 'use strict';
 
-var renderModel = require('../../../app/templates/renderModel');
 var mongoose = require('mongoose');
 
 // Compile Schema into Model here
 var AffiliateApp = mongoose.model('affiliateAppointments');
 
-/*
-Populates the database with test data
-*/
-function dummyObject(Model) {
-	var obj = new Model({
-		app: 'this is some nonsensical jibberish'
-	});
-	return obj;
-}
+var modelClass = require('../modelClass');
+var renderModel = new modelClass.RenderModel( AffiliateApp, 'affiliateAppointments/affiliateAppointments.tex', 'affiliateAppointments/na.tex');
 
 /*
-Helper function that gets called in report.server.controller.js
-Output is pushed into a LaTex PDF there.
+will explicitly populate the report with
+the data you provide
 */
-module.exports.render = function (callback) {
-	renderModel.render( 'affiliateAppointments/affiliateAppointments.tex', AffiliateApp, dummyObject, function ( renderStr ) {
-		callback(null, renderStr);
+renderModel.setDebugPopulate( false, {
+	app: 'whatevs, bro'
+});
+
+/*
+will explicitly print the N/A latex
+to the screen for debugging purposes
+*/
+renderModel.isDebugNull = false;
+
+/*
+render function that finds the obj in the database
+and converts it into latex.
+*/
+module.exports.render = function(req, callback) {
+	renderModel.findOneModelByReport( req, function( obj ) {
+		renderModel.render( obj, callback );
 	});
 };
+
+/*
+//Exactly the same as the render above, but 
+//uses the fidnModelsByReport, which returns
+//an array of JSON objects
+
+module.exports.render = function(req, callback) {
+	renderModel.findModelsByReport( req, function( arrayOfObjs ) {
+		return arrayOfObjs[0];
+	}, function( single_obj ) {
+		renderModel.render( single_obj, callback );
+	});
+};
+*/
 
 module.exports.submit = function(req, callback) {
 	var affApp = new AffiliateApp({
