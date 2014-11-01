a4e7aa46b0f5b057751515edb6a8a0eeb45b7304
Ian Fell <ifell@ufl.edu>
2014-11-01 11:41:00 -0400

    changed how public/private fields work in modelClass

--- app/tests/controllers/modelClass.server.controller.test.js
+++ app/tests/controllers/modelClass.server.controller.test.js
@@ -2,3 +2,19 @@
 
 var should = require('should'),
 	modelClass = require('../../templates/modelClass');
+
+describe('ModelClass Controller Tests', function() {
+	beforeEach(function(done) {
+		done();
+	});
+
+	describe('', function() {
+		it('', function(done) {
+
+		});
+	});
+
+	afterEach(function(done) {
+
+	});
+});
