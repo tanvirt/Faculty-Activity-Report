2f768a29890c40f39a63476780ed61bbcf52bf63
Christine King <airpwane@ufl.edu>
2014-11-03 11:56:39 -0500

    Update report.server.controller.js

--- app/controllers/report.server.controller.js
+++ app/controllers/report.server.controller.js
@@ -149,7 +149,7 @@ exports.submit = function(req, res, next) {
 		async.apply(renderInternational.submit, req),
 		async.apply(renderMembership.submit, req),
 		async.apply(renderConferences.submit, req),
-		async.apply(renderConsultationsOutsideUniversity.submit, req)
+	//	async.apply(renderConsultationsOutsideUniversity.submit, req)
 		
 		
 		
@@ -175,9 +175,9 @@ exports.submit_02 = function(req, res, callback) {
 		teachingEvaluation: async.apply(renderTeachingEvaluation.submit, req),
 		conferences: async.apply(renderConferences.submit, req),
 		graduateCommittee: async.apply(renderGraduateCommittee.submit, req),
-		creativeWorks: async.apply(renderCreativeWorks.submit, req),
+		creativeWorks: async.apply(renderCreativeWorks.submit, req)
 	//	honors: async.apply(renderHonors.submit, req),
-		async.apply(renderConsultationsOutsideUniversity.submit, req)
+	//	async.apply(renderConsultationsOutsideUniversity.submit, req),
 		
 		
 	}, function(err, models) {
