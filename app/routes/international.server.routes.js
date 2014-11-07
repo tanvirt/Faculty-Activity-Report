2d1fd7025f092b1b82e220609cd6781fa73a1389
Greg <friedmangreg37@ufl.edu>
2014-11-07 13:25:48 -0500

    added controller for international activities section

---
+++ app/routes/international.server.routes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(app) {
+	var users = require('../../app/controllers/users');
+	var international = require('../../app/controllers/international/international');
+	//var reports = require('../../app/controllers/reports');
+
+	//todo: require authorization
+	app.route('/reports/:reportId/international')
+		.get(users.requiresLogin, international.readFromReport)
+		.post(users.requiresLogin, international.create);
+
+	app.route('/reports/:reportId/international/:internationalId')
+		.get(international.read)
+		.put(users.requiresLogin, international.update);
+
+	// Finish by binding the AssignedActivity middleware
+	app.param('international', international.internationalById);
+};
