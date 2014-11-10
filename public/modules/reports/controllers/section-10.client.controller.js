9629726deba403011e30fbac39dd27c33b26e8b0
tanvirt <tanvirt@ufl.edu>
2014-11-10 18:09:17 -0500

    centralized tinymce, altered controller parameters

--- public/modules/reports/controllers/section-10.client.controller.js
+++ public/modules/reports/controllers/section-10.client.controller.js
@@ -4,19 +4,6 @@ angular.module('reports').controller('Section10Controller', ['$scope', '$statePa
 	function($scope, $stateParams, $location, Authentication, Reports ) {
 		$scope.authentication = Authentication;
 		
-		//custom tinymce textarea
-		$scope.tinymceOptions = {
-			    theme: 'modern',
-			    plugins: [
-			        'autoresize',
-			        'advlist autolink lists charmap preview hr',
-			        'searchreplace wordcount',
-			        'insertdatetime save table contextmenu directionality',
-			        'paste textcolor colorpicker textpattern',
-			    ],
-			    toolbar1: 'undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist',
-		};
-		
 	}
 ]);
 
--- public/modules/reports/controllers/section-13.client.controller.js
+++ public/modules/reports/controllers/section-13.client.controller.js
@@ -1,20 +1,8 @@
 'use strict';
 
-angular.module('reports').controller('Section13Controller', ['$scope',
-	function($scope) {
-		
-		//custom tinymce textarea
-		$scope.tinymceOptions = {
-			    theme: 'modern',
-			    plugins: [
-			        'autoresize',
-			        'advlist autolink lists charmap preview hr',
-			        'searchreplace wordcount',
-			        'insertdatetime save table contextmenu directionality',
-			        'paste textcolor colorpicker textpattern',
-			    ],
-			    toolbar1: 'undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist',
-		};
+angular.module('reports').controller('Section13Controller', ['$scope', '$stateParams', '$location', 'Authentication',
+	function($scope, $stateParams, $location, Authentication, Reports ) {
+		$scope.authentication = Authentication;
 
 	}
 ]);
