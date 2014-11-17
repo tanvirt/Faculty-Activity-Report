8bdbafcf4b4edc01e0623e7920de298f355f1afd
unknown <aknoxlor@gmail.com>
2014-11-17 13:04:37 -0500

    Added Further Information Section route

---
+++ app/routes/furtherInformationSection.server.routes.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(app) {
+	var users = require('../../app/controllers/users');
+	var furtherInformationSection = require('../../app/controllers/furtherInformationSection/furtherInformationSection');
+
+	//todo: require authorization
+	app.route('/reports/:reportId/furtherInformationSection')
+		.get(users.requiresLogin, furtherInformationSection.readFromReport)
+		.post(users.requiresLogin, furtherInformationSection.create);
+
+	app.route('/furtherInformationSection/:assignedActivityId')
+		.get(users.requiresLogin, furtherInformationSection.read)
+		.put(users.requiresLogin, furtherInformationSection.update);
+
+	// Finish by binding the FurtherInformationSection middleware
+	app.param('assignedActivityId', furtherInformationSection.furtherInformationSectionById);
+};
