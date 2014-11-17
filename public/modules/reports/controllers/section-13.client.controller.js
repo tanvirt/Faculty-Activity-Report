20242d3a076d8e19a7d6a71d1c8e97296047b95e
dafefuve <dafefuve@AC80033C.ipt.aol.com>
2014-11-16 21:38:03 -0500

    [Modified/Created #79492360] functionality to modified the backend is defiend but backend is still undefined

--- public/modules/reports/controllers/section-13.client.controller.js
+++ public/modules/reports/controllers/section-13.client.controller.js
@@ -1,8 +1,29 @@
 'use strict';
 
-angular.module('reports').controller('Section13Controller', ['$scope', '$stateParams', '$location', 'Authentication',
-	function($scope, $stateParams, $location, Authentication, Reports ) {
+angular.module('reports').controller('Section13Controller', ['$http', '$scope', '$location', 'Authentication', 'IDs',
+	function($http, $scope, $location, Authentication, IDs ) {
 		$scope.authentication = Authentication;
+
+		IDs.get().then(function(data) {
+
+			$scope.IDdata = data;
+			$scope.section13 = data.publications.info;
+		});
+		$scope.update = function()
+		{
+			$http.put('/publications/' + $scope.IDdata.publications._id, {
+
+				publications:{
+					info: $scope.section13
+				}}).
+				success(function(data, status, headers, config) {
+					alert('Saved!');
+				}).
+				error(function(data, status, headers, config) {
+					alert('There was an error Saving!');
+				}
+			);
+		};
 
 	}
 ]);
