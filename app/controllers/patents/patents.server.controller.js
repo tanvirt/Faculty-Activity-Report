8e9f8af09cce301fa117f0598b70d98650c42b6f
mkwheeler <mkwheeler@ufl.edu>
2014-11-17 23:12:09 -0500

    only p1 read in GET

---
+++ app/controllers/patents/patents.server.controller.js
@@ -0,0 +1,99 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Patents = mongoose.model('Patents');
+
+var errorHandler = require('../errors');
+var is = require('is-js');
+var _ = require('lodash');
+
+/*
+Gets the data from the frontend and
+saves it in the database.
+*/
+
+exports.create = function(req, res) {
+	if (is.empty(req.body.patents)) {
+		return res.jsonp({
+			err: 'Post (create): Does not exist',
+			message: 'req.body.patents did not get send to backend',
+			changes: 'No Patents Created'
+		});
+	}
+
+	var patents = new Patents({
+		name: req.body.patents.name,
+		description: req.body.patents.description,
+		website: req.body.patents.website,
+		jointEfforts: req.body.patents.jointEfforts,
+		date: req.body.patents.date,
+
+		user: req.user,
+		report: req.report
+	});
+
+	patents.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			req.report.patents = patents;
+			req.report.save();
+
+			res.jsonp(patents);
+		}
+	});
+};
+
+exports.update = function(req, res) {
+	console.log(require('util').inspect(req.body));
+	
+	if (is.empty(req.body.patents)) {
+		res.status(400);
+		return res.jsonp({
+			err: 'Put (update): Does not exist',
+			message: 'req.body.patents did not get send to backend',
+			changes: 'No Changes Made'
+		});
+	}
+
+	var patents = req.patents;
+
+	patents = _.extend(patents, req.body.patents);
+
+	patents.save(function(err) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			res.jsonp(patents);
+		}
+	});
+};
+
+exports.readFromReport = function(req, res) {
+	Patents.find({report: req.report}, function(err, result) {
+		if (err) {
+			return res.status(400).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		}
+		return res.jsonp(result);
+	});
+};
+
+exports.read = function(req, res) {
+	res.jsonp(req.patents);
+};
+
+exports.patentsById = function(req, res, next, id) {
+	Patents.findById(id)
+	.exec(function(err, patents) {
+		if (err) return next(err);
+		if (!patents) return next(new Error('Failed to load Patents ' + id));
+		req.patents = patents;
+		next();
+	});
+};
---
+++ app/tests/controllers/patents.server.controller.test.js
@@ -0,0 +1,274 @@
+'use strict';
+
+/*jshint expr: true*/
+
+var app = require('../../../server');
+
+var should = require('should');
+var request = require('supertest');
+
+var	patents = require('../../controllers/patents/patents');
+
+var mongoose = require('mongoose');
+var Patents = mongoose.model('Patents');
+
+var User = mongoose.model('User');
+var Report = mongoose.model('Report');
+
+var async = require('async');
+
+var user, report, p1, p2, p3;
+
+describe('Patents Controller Tests', function() {
+	beforeEach(function(done) {
+
+		user = new User({
+			firstName: 'Full',
+			lastName: 'Name',
+			email: 'test@test.com',
+			username: 'username',
+			password: 'password',
+			provider: 'local'
+		});
+
+		user.save();
+
+		report = new Report({
+			reportName: 'MyReportName',
+			user: user
+		});
+
+		report.save();
+
+		p1 = new Patents({
+			title: 'New',
+			authors: ['Me', 'Myself', 'I'],
+			patentNumber: '1234',
+			date: '10/07/2014',
+			description: 'I wrote this book thingy all by myself and it is mine and you cannot have it hahahahahahahaha',
+		
+			report: report,
+			user: user
+		});
+
+		p2 = new Patents({
+			title: 'Alphabet',
+			authors: ['A', 'B', 'C'],
+			patentNumber: 'defg',
+			date: '03/12/2000',
+			description: 'hijklmnop',
+		
+
+			report: report,
+			user: user
+		});
+
+		p3 = new Patents({
+			title: 'three',
+			authors: ['one', '2', 'three'],
+			patentNumber: '4380jfs084',
+			date: '05/02/1999',
+			description: 'lalalalalalala laklafnlkwnf',
+		
+			report: report,
+			user: user
+		});
+
+		p1.save();
+		p2.save();
+		p3.save();
+
+		done();
+	});
+
+	describe('Testing the GET methods', function() {
+
+		it('should fail to be able to get a patents if not logged in', function(done) {
+			request(app)
+			  .get('/reports/' + report.id + '/patents')
+			  .set('Accept', 'application/json')
+			  .expect('Content-Type', /json/)
+			  .expect(401)
+			  .end(done);
+		});
+
+		it('should be able to get an patents associated with its report id', function(done) {
+			request(app)
+				.post('/auth/signin')
+				.send({
+					username:'username',
+					password:'password'
+				})
+				.expect(200)
+				.end(function(err, res) {
+					request(app)
+						.get('/reports/' + report.id + '/patents')
+						.set('cookie', res.headers['set-cookie'])
+						.set('Accept', 'application/json')
+						.expect('Content-Type', /json/)
+						.expect(200)
+						.end(function(err, res) {
+							should.not.exist(err);
+
+							res.body.should.be.an.Array;
+							res.body.should.have.length(3);
+
+							res.body[0].should.be.an.Object;	
+							res.body[1].should.be.an.Object;	
+							res.body[2].should.be.an.Object;							
+
+						  	done();
+						});
+				});
+		});
+
+		it('should fail to be able to get a specific patents if not logged in', function(done) {
+			request(app)
+			  .get('/patents/' + p1.id)
+			  .set('Accept', 'application/json')
+			  .expect('Content-Type', /json/)
+			  .expect(401)
+			  .end(done);
+		});
+
+		it('should be able to get a specific patents based on its id', function(done) {
+			request(app)
+				.post('/auth/signin')
+				.send({
+					username:'username',
+					password:'password'
+				})
+				.expect(200)
+				.end(function(err, res) {
+					request(app)
+					  .get('/patents/' + p1.id)
+					  .set('cookie', res.headers['set-cookie'])
+					  .set('Accept', 'application/json')
+					  .expect('Content-Type', /json/)
+					  .expect(200)
+					  .end(function(err, res) {
+					  	should.not.exist(err);
+
+					  	res.body.should.be.an.Object;
+
+					  	res.body.should.have.property('_id', p1.id);
+					  	res.body.should.have.property('user', user.id);
+					  	res.body.should.have.property('report', report.id);
+
+					  	done();
+					  });
+				});
+		});
+
+	});
+
+	describe('Testing the POST methods', function() {
+
+		var pObj = {
+			patents: {
+			    title: 'new patent',
+				authors: ['albus', 'gandalf', 'the queen of york'],
+				patentNumber: '8374SJF4GH93',
+				date: '03/22/2004',
+				description: 'the desc'
+		 	}
+		};
+
+		it('should fail to be able to create an patents if not logged in', function(done) {
+			request(app)
+			  .post('/reports/' + report.id + '/patents')
+			  .set('Accept', 'application/json')
+			  .send(pObj)
+			  .expect('Content-Type', /json/)
+			  .expect(401)
+			  .end(done);
+		});
+
+		it('should be able to create a new patents', function(done) {
+			request(app)
+				.post('/auth/signin')
+				.send({
+					username:'username',
+					password:'password'
+				})
+				.expect(200)
+				.end(function(err, res) {
+					request(app)
+					  .post('/reports/' + report.id + '/patents')
+					  .set('cookie', res.headers['set-cookie'])
+					  .set('Accept', 'application/json')
+					  .send(pObj)
+					  .expect('Content-Type', /json/)
+					  .expect(200)
+					  .end(function(err, res) {
+					  	should.not.exist(err);
+
+					  	res.body.should.have.property('name', pObj.patents.name);
+
+					  	res.body.should.have.property('_id');
+					  	res.body.should.have.property('user');
+					  	res.body.should.have.property('report');
+
+					  	done();
+					  });
+				});
+		});
+
+	});
+
+	describe('Testing the PUT methods', function() {
+
+		it('should fail to be able to update a specific patents if not logged in', function(done) {
+			request(app)
+			  .put('/patents/' + p1.id)
+			  .set('Accept', 'application/json')
+			  .expect('Content-Type', /json/)
+			  .expect(401)
+			  .end(done);
+		});
+
+		it('should be able to update a specific patents', function(done) {
+			request(app)
+				.post('/auth/signin')
+				.send({
+					username:'username',
+					password:'password'
+				})
+				.expect(200)
+				.end(function(err, res) {
+					request(app)
+					.put('/patents/' + p1.id)
+					.set('cookie', res.headers['set-cookie'])
+				  	.set('Accept', 'application/json')
+				  	.send({
+				  		patents: {
+				  			title:'DifferentName'
+				  		}
+				  	})
+				  	.expect('Content-Type', /json/)
+				  	.expect(200)
+				  	.end(function(err, res) {
+				  		should.not.exist(err);
+
+					  	res.body.should.be.an.Object.and.have.property('name', 'DifferentName');
+
+					  	res.body.should.have.property('_id', p1.id);
+					  	res.body.should.have.property('user', user.id);
+					  	res.body.should.have.property('report', report.id);
+
+				  		done();
+				  	});
+
+				});
+		});
+			
+	    
+	});
+
+	afterEach(function(done) {
+		Patents.remove().exec();
+		User.remove().exec();
+		Report.remove().exec();
+		done();
+	});
+});
