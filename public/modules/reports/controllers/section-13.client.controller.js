8f6eec417f7a6567a8f9d6b16108f157cef45dca
dafefuve <dafefuve@DAFEFUVE-MACPRO.local>
2014-12-06 16:28:12 -0500

    [#83172762] dblp will not be using http request form the fron end to support functionality instead will be parsing xml inputed by the user

--- public/modules/reports/controllers/section-13.client.controller.js
+++ public/modules/reports/controllers/section-13.client.controller.js
@@ -1,8 +1,22 @@
 'use strict';
 
-angular.module('reports').controller('Section13Controller', ['$http', '$scope', '$location', 'Authentication', 'IDs',
-	function($http, $scope, $location, Authentication, IDs ) {
+angular.module('reports').controller('Section13Controller', ['$http', '$scope', '$location', 'Authentication', 'IDs', 'x2js',
+	function($http, $scope, $location, Authentication, IDs, x2js ) {
 		$scope.authentication = Authentication;
+		console.log($scope.frame);
+		$scope.search = function(){
+//   http://dblp.uni-trier.de/rec/pers/d/Dobra:Alin/xk
+			$http.get('http://www.html5rocks.com/en/tutorials/file/xhr2/',{
+				}).
+				success(function(data, status, headers, config) {
+				console.log(data);
+			}).
+				error(function(data, status, headers, config) {
+					// called asynchronously if an error occurs
+					// or server returns response with an error status.
+				});
+
+		};
 		
 		IDs.get().then(function(data) 
 		{
@@ -24,5 +38,6 @@ angular.module('reports').controller('Section13Controller', ['$http', '$scope', 
 				}
 			);
 		};
+
 	}
 ]);
