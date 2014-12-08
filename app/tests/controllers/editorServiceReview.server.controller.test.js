fae7b660bdff3747f35f560331660c89d136e52d
Greg <friedmangreg37@ufl.edu>
2014-12-07 23:40:49 -0500

    Small changes to previously done authorizations to fix them

--- app/tests/controllers/editorServiceReview.server.controller.test.js
+++ app/tests/controllers/editorServiceReview.server.controller.test.js
@@ -101,10 +101,10 @@ describe('EditorServiceReviewer Controller Tests', function() {
 						.end(function(err, res) {
 							should.not.exist(err);
 
-							res.body[0].should.have.property('_id', e1.id);
-							res.body[0].should.have.property('info', e1.info);
-						  	res.body[0].user.should.have.property('_id', user.id);
-						  	res.body[0].report.should.have.property('_id', report.id);
+							res.body.should.have.property('_id', e1.id);
+							res.body.should.have.property('info', e1.info);
+						  	res.body.user.should.have.property('_id', user.id);
+						  	res.body.report.should.have.property('_id', report.id);
 						  	done();
 						});
 				});
